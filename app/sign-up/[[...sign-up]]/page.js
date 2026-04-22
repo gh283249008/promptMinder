a7803eca3e569a27cd1c61e13741c3dfb832dd42
gh283249008 <283249008@qq.com>
2026-04-22 13:25:54 +0800

    修复: sign-in/sign-up 页面改为跳转到 admin-login

--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -1,9 +1,5 @@
-import { SignUp } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return (
-    <div className="relative min-h-screen flex items-center justify-center px-4 py-10 bg-gradient-to-br from-sky-50 via-white to-emerald-50 dark:from-zinc-900 dark:via-black dark:to-zinc-900">
-      <SignUp forceRedirectUrl="/prompts" />
-    </div>
-  );
+  redirect("/admin-login");
 }
